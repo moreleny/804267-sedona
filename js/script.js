29e53c4df45b22e869ff06a07302aad6e9a46aef
moreleny <42089697+moreleny@users.noreply.github.com>
2018-09-05 21:16:56 +0300

    Доделала верстку

---
+++ js/script.js
@@ -0,0 +1,28 @@
+var btnlink = document.querySelector(".btn-search");
+var popup = document.querySelector(".search-form");
+var headerlink = document.querySelector(".main-header-link");
+var sortingtypelink = document.querySelector(".sorting-type-link");
+var btnarrowlink = document.querySelectorAll(".btn-up-svgpath, .btn-down-svgpath");
+
+btnlink.addEventListener("click", function(evt) {
+	
+	event.preventDefault();
+	popup.classList.toggle("popup-down");
+	
+});													
+
+/* КОД, КОТОРЫЙ НЕ СМОГ(  
+headerlink.addEventListener("click", function(evt) {
+	headerlink.classList.add("active-link");
+});		
+
+sortingtypelink.addEventListener("click", function(evt) {
+	event.preventDefault();
+	sortingtypelink.classList.add("active-sorting-type-link");
+});		
+
+btnarrowlink.addEventListener("click", function(evt) {
+	event.preventDefault();
+	btnarrowlink.classList.add("btn-arrow-svgpath-active");
+});	
+*/
